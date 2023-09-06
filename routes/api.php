d53163492fdba90efbc633b5399a83723b3dc381
hltav <hudsonlimatavares@gmail.com>
2023-09-06 11:49:17 -0300

    config api and key env

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,19 @@
 <?php
 
-// use App\Http\Controllers\GithubUserController;
-// use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\Route;
-
-//     Route::get('users', [GithubUserController::class, 'index']);
-//     Route::post('users', [GithubUserController::class, 'store']);
-//     Route::put('users/{user}', [GithubUserController::class, 'update']);
-//     Route::delete('users/{user}', [GithubUserController::class, 'destroy']);
-
 use App\Http\Controllers\GithubUserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('cors')->group(function () {
     Route::get('users', [GithubUserController::class, 'index']);
     Route::post('users', [GithubUserController::class, 'store']);
     Route::put('users/{user}', [GithubUserController::class, 'update']);
     Route::delete('users/{user}', [GithubUserController::class, 'destroy']);
-});
+
+// use App\Http\Controllers\GithubUserController;
+// use Illuminate\Support\Facades\Route;
+
+// Route::middleware('cors')->group(function () {
+//     Route::get('users', [GithubUserController::class, 'index']);
+//     Route::post('users', [GithubUserController::class, 'store']);
+//     Route::put('users/{user}', [GithubUserController::class, 'update']);
+//     Route::delete('users/{user}', [GithubUserController::class, 'destroy']);
+// });
